f63ed247e5da8ac507d14a9a5ae9ec7f89a743f9
Hasif <hasif@annikken.com>
2018-05-22 10:52:12 +0800

    initial commit

---
+++ src/AndeeESP32.cpp
@@ -0,0 +1,8 @@
+// AndeeESP32.cpp - Arduino 101 Library
+// Annikken Pte Ltd
+// Author: Muhammad Hasif
+
+void AndeeBT::begin(const char* name)
+{
+	
+}
---
+++ src/AndeeESP32.h
@@ -0,0 +1,10 @@
+// AndeeESP32.h - Arduino 101 Library
+// Annikken Pte Ltd
+// Author: Muhammad Hasif
+
+class AndeeBT
+{
+	public:
+	void begin(const char*);
+	
+}
